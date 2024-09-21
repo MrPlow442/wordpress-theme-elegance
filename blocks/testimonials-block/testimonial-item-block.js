e4d07cb0561d6d176b30658606c1358f8bc0e013
Matija Lovrekovic <mlovrekov@gmail.com>
2024-09-21 16:06:36 +0200

    Added image for testimonial

--- blocks/testimonials-block/testimonial-item-block.js
+++ blocks/testimonials-block/testimonial-item-block.js
@@ -25,6 +25,10 @@ wp.blocks.registerBlockType('elegance-theme/testimonial-item-block', {
             source: 'html',
             selector: 'span',
         },
+        imageUrl: { // Add an attribute for the image URL
+            type: 'string',
+            default: '', // Default to empty string if no image is uploaded
+        },
     },
     supports: {
         inserter: false
@@ -32,24 +36,36 @@ wp.blocks.registerBlockType('elegance-theme/testimonial-item-block', {
 
     edit: (props) => {
         const { attributes, setAttributes } = props;
+        const { name, quote, position, imageUrl } = attributes;
         const blockProps = wp.blockEditor.useBlockProps({ className: 'item animate', 'data-animate': 'fadeInUp' });
 
         return wp.element.createElement('div', { ...blockProps },
             wp.element.createElement('div', { className: 'testimonial-item' },
+                // Add the image upload section
+                imageUrl && wp.element.createElement('div', { className: 'client-row' },
+                    wp.element.createElement('img', { src: imageUrl, className: 'rounded-circle', alt: 'Client image' })
+                ),
+                wp.element.createElement(wp.blockEditor.MediaUpload, {
+                    onSelect: (media) => setAttributes({ imageUrl: media.url }),
+                    allowedTypes: ['image'],
+                    render: ({ open }) => wp.element.createElement(wp.components.Button, { onClick: open },
+                        imageUrl ? 'Change Image' : 'Upload Image'
+                    ),
+                }),
                 wp.element.createElement('div', { className: 'testimonial-content' },
                     wp.element.createElement(wp.blockEditor.RichText, { 
                         tagName: 'h4', 
-                        value: attributes.name, 
+                        value: name, 
                         placeholder: 'Name', 
                         onChange: (value) => setAttributes({ name: value })}),
                     wp.element.createElement(wp.blockEditor.RichText, { 
                         tagName: 'p', 
-                        value: attributes.quote, 
+                        value: quote, 
                         placeholder: 'Quote', 
                         onChange: (value) => setAttributes({ quote: value })}),
                     wp.element.createElement(wp.blockEditor.RichText, { 
                         tagName: 'span', 
-                        value: attributes.position, 
+                        value: position, 
                         placeholder: 'Position', 
                         onChange: (value) => setAttributes({ position: value })})
                 )
@@ -58,13 +74,19 @@ wp.blocks.registerBlockType('elegance-theme/testimonial-item-block', {
     },
 
     save: (props) => {
-        const { attributes, setAttributes } = props;
+        const { attributes } = props;
+        const { name, quote, position, imageUrl } = attributes;
+
         return wp.element.createElement('div', { className: 'item animate', 'data-animate': 'fadeInUp' },
             wp.element.createElement('div', { className: 'testimonial-item' },
+                // Conditionally render the image
+                imageUrl && wp.element.createElement('div', { className: 'client-row' },
+                    wp.element.createElement('img', { src: imageUrl, className: 'rounded-circle', alt: 'Client image' })
+                ),
                 wp.element.createElement('div', { className: 'testimonial-content' },
-                    wp.element.createElement(wp.blockEditor.RichText.Content, { tagName: 'h4', value: attributes.name }),
-                    wp.element.createElement(wp.blockEditor.RichText.Content, { tagName: 'p', value: attributes.quote }),
-                    wp.element.createElement(wp.blockEditor.RichText.Content, { tagName: 'span', value: attributes.position })
+                    wp.element.createElement(wp.blockEditor.RichText.Content, { tagName: 'h4', value: name }),
+                    wp.element.createElement(wp.blockEditor.RichText.Content, { tagName: 'p', value: quote }),
+                    wp.element.createElement(wp.blockEditor.RichText.Content, { tagName: 'span', value: position })
                 )
             )
         );
